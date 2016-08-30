9f56aee596ca49713f0e5d4bdc4035286b8f5b5f
Your Name <you@example.com>
2016-08-30 11:09:11 -0500

    Se agrega chequeo

--- estoca.jl
+++ estoca.jl
@@ -1,5 +1,4 @@
 
-
 function estoca(n,m)
        A=zeros(Float64,n,m);
        for i = 1:n
@@ -14,3 +13,24 @@ function estoca(n,m)
        end
        print(A);
 end
+
+function prueba(A)
+	dim=size(A);
+	check=0;
+	for i=1:dim[1]
+		sum=0;
+		for j=1:dim[2]
+			sum=sum+A[i,j];
+		end
+		if sum==1
+			check=check+1;
+		end
+	end
+	if check==dim[1]
+		print("Es estocástica");
+	else
+		print("No es estocástica");
+	end
+end
+		
+	
