88d4f18353d5f6746fb4ee69eb076acb7527c044
Your Name <you@example.com>
2016-08-30 11:49:38 -0500

    hermitina.jl calcula eigenvalores

--- hermitiana.jl
+++ hermitiana.jl
@@ -1,5 +1,8 @@
-a=rand(10,10)+1im*rand(10,10)
-b=triu(a,1)
-c=transpose(conj(b))+b
+function matriz_aleatoria(n)
+a=rand(n,n)+1im*rand(n,n);
+b=triu(a,1);
+c=transpose(conj(b))+b;
+eigvals(c)
+end;
 
 #La diagonal debe ser con numeros reales
