dc02907b1de5b958b46029cd249ebcbdf955ff37
LGNR <lgnr.archive@gmail.com>
2016-10-10 22:17:00 -0500

    Update tarea5.jl

--- tarea5.jl
+++ tarea5.jl
@@ -1,1 +1,31 @@
+using quantum
+using Base.Test
 
+"""
+Traza parcial 
+"""
+function partial_trace_pure_bipartite_mat(state,dim,system)
+    dimtotal=length(state)[1]
+    dimcomp=Int(dimtotal/dim)
+    if system==1
+    psi=reshape(state,(dimcomp,dim))'
+        return psi*psi'
+        elseif system==2
+     psi=reshape(state,(dim,dimcomp))'
+        return psi'*psi
+    end
+end
+
+"""
+Checa estado aleatorio
+"""
+
+function checa()
+  state_A=random_state(3);  
+  state_B=random_state(5);
+  stateAB=kron(state_A,state_B);  
+  trace(partial_trace_pure_bipartite_mat(stateAB,3,1))
+  trace(partial_trace_pure_bipartite_mat(stateAB,5,2))
+end
+
+@test checa()
